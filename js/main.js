7add4174214f14a804113a9d0d8ca0bbd87540e1
khari1090 <khari1090@gmail.com>
2016-12-10 23:28:16 +0100

    modal added

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// When the user clicks the button, open the modal
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+    document.getElementById('modal-1').style.display = "block";
+    document.getElementById('modal-email').value = "";
+    document.getElementById('modal-2').style.display = "none";
+    document.getElementById('modal-3').style.display = "none";
+    
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal')) {
+        document.getElementById('myModal').style.display = "none";
+    }
+}
+
+function submitEmail(){
+  checkInput();
+}
+
+function checkInput(){
+  var input = document.getElementById('modal-email');
+  if (input.value == null || input.value == ""){
+    document.getElementById('modal-email').style.backgroundColor = "#ffdad3";
+    document.getElementById('modal-email').style.border = "1px solid tomato";
+    return;
+  }
+  else {
+    document.getElementById('modal-email').style.backgroundColor = "";
+    document.getElementById('modal-email').style.border = "";
+    payment();
+    return;
+  }
+}
+
+function payment(){
+  document.getElementById('modal-1').style.display = "none";
+  document.getElementById('modal-3').style.display = "none";
+  document.getElementById('modal-2').style.display = "block";
+}
+
+function creditCard(){
+  document.getElementById('modal-1').style.display = "none";
+  document.getElementById('modal-2').style.display = "none";
+  document.getElementById('modal-3').style.display = "block";
+}
