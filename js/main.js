8a6a01ba3ae20202d6ec490e9b80208bd35e2e49
khari1090 <khari1090@gmail.com>
2016-12-11 16:12:33 +0100

    ok

--- js/main.js
+++ js/main.js
@@ -1,24 +1,31 @@
 // When the user clicks the button, open the modal
-function openModal() {
-    document.getElementById('myModal').style.display = "block";
-    document.getElementById('modal-1').style.display = "block";
-    document.getElementById('modal-email').value = "";
-    document.getElementById('modal-2').style.display = "none";
-    document.getElementById('modal-3').style.display = "none";
-    
+function openModal(){
+  document.getElementById('myModal').style.display = "block";
+  document.getElementById('modal-1').style.display = "block";
+  document.getElementById('modal-email').value = "";
+  document.getElementById('modal-2').style.display = "none";
+  document.getElementById('modal-3').style.display = "none";
+  console.log("ok");
 }
 
 // When the user clicks on <span> (x), close the modal
 function closeModal() {
-    document.getElementById('myModal').style.display = "none";
+  document.getElementById('myModal').style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == document.getElementById('myModal')) {
-        document.getElementById('myModal').style.display = "none";
-    }
-}
+// window.onclick = function(event) {
+//   if (event.target == document.getElementById('myModal')) {
+//     document.getElementById('myModal').style.display = "none";
+//   }
+// }
+
+// window.onload = function() {
+//   document.getElementById('modal-2').style.display = "none";
+//   document.getElementById('modal-3').style.display = "none";
+//   document.getElementById('myModal').style.display = "none";
+//   document.getElementById('modal-1').style.display = "none";
+// }
 
 function submitEmail(){
   checkInput();
@@ -41,8 +48,8 @@ function checkInput(){
 
 function payment(){
   document.getElementById('modal-1').style.display = "none";
-  document.getElementById('modal-3').style.display = "none";
   document.getElementById('modal-2').style.display = "block";
+  document.getElementById('modal-3').style.display = "none";
 }
 
 function creditCard(){
