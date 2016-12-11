edfe12e94c01528338029a9005d97632c2e7f9ae
khari1090 <khari1090@gmail.com>
2016-12-11 17:59:54 +0100

    sdjoij

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ function openModal(){
   document.getElementById('modal-email').value = "";
   document.getElementById('modal-2').style.display = "none";
   document.getElementById('modal-3').style.display = "none";
-  console.log("ok");
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -14,18 +13,11 @@ function closeModal() {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == document.getElementById('myModal')) {
-//     document.getElementById('myModal').style.display = "none";
-//   }
-// }
-
-// window.onload = function() {
-//   document.getElementById('modal-2').style.display = "none";
-//   document.getElementById('modal-3').style.display = "none";
-//   document.getElementById('myModal').style.display = "none";
-//   document.getElementById('modal-1').style.display = "none";
-// }
+window.onclick = function(event) {
+  if (event.target == document.getElementById('myModal')) {
+    document.getElementById('myModal').style.display = "none";
+  }
+}
 
 function submitEmail(){
   checkInput();
@@ -34,13 +26,13 @@ function submitEmail(){
 function checkInput(){
   var input = document.getElementById('modal-email');
   if (input.value == null || input.value == ""){
-    document.getElementById('modal-email').style.backgroundColor = "#ffdad3";
-    document.getElementById('modal-email').style.border = "1px solid tomato";
+    input.style.backgroundColor = "#ffdad3";
+    input.style.border = "1px solid tomato";
     return;
   }
   else {
-    document.getElementById('modal-email').style.backgroundColor = "";
-    document.getElementById('modal-email').style.border = "";
+    input.style.backgroundColor = "";
+    input.style.border = "";
     payment();
     return;
   }
