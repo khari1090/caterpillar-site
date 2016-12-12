e03dcc310c961e6324d849ea647cb2b26756aa71
khari1090 <khari1090@gmail.com>
2016-12-12 21:43:05 +0100

    added another section, changed colors, added final cta

--- js/main.js
+++ js/main.js
@@ -49,3 +49,40 @@ function creditCard(){
   document.getElementById('modal-2').style.display = "none";
   document.getElementById('modal-3').style.display = "block";
 }
+
+function singleUseToken() {
+  var form = document.getElementById('payment-form');
+  form.submit(function(event) {
+    // Disable the submit button to prevent repeated clicks:
+    form.find('.submit').prop('disabled', true);
+
+    // Request a token from Stripe:
+    Stripe.card.createToken(form, stripeResponseHandler);
+
+    // Prevent the form from being submitted:
+    return false;
+  });
+};
+
+function stripeResponseHandler(status, response) {
+  // Grab the form:
+  var form = document.getElementById('payment-form');
+
+  if (response.error) { // Problem!
+
+    // Show the errors on the form:
+    form.find('.payment-errors').text(response.error.message);
+    form.find('.submit').prop('disabled', false); // Re-enable submission
+
+  } else { // Token was created!
+
+    // Get the token ID:
+    var token = response.id;
+
+    // Insert the token ID into the form so it gets submitted to the server:
+    form.appendChild(('<input type="hidden" name="stripeToken">').val(token));
+
+    // Submit the form:
+    form.get(0).submit();
+  }
+};
