10df51c816f3c7fb1c181e940dc7a173f2ec2b4a
khari1090 <khari1090@gmail.com>
2017-01-20 11:08:40 +0100

    working with firebase

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 function openModal(){
   document.getElementById('myModal').style.display = "block";
   document.getElementById('modal-1').style.display = "block";
-  document.getElementById('modal-email').value = "";
+  document.getElementById('txtEmail').value = "";
+  document.getElementById('txtPassword').value = "";
+  document.getElementById('errorMessage').value = "";
   document.getElementById('modal-2').style.display = "none";
   document.getElementById('modal-3').style.display = "none";
 }
@@ -12,77 +14,43 @@ function closeModal() {
   document.getElementById('myModal').style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == document.getElementById('myModal')) {
-    document.getElementById('myModal').style.display = "none";
-  }
-}
+// When the user clicks anywhere outside of the modal, close the modal
+// window.onclick = function(event) {
+//   if (event.target == document.getElementById('myModal')) {
+//     document.getElementById('myModal').style.display = "none";
+//   }
+// }
 
-function submitEmail(){
-  checkInput();
-}
+// function submitNewUser(){
+//   checkInput();
+// }
+//
+// function checkInput(){
+//   var email = document.getElementById('txtEmail');
+//   var pass = document.getElementById('txtPassword');
+//   if (email.value == null || input.value == "" || pass.value == null || pass.value == ""){
+//     email.style.backgroundColor = "#ffdad3";
+//     email.style.border = "1px solid tomato";
+//     pass.style.backgroundColor = "#ffdad3";
+//     pass.style.border = "1px solid tomato";
+//     return;
+//   }
+//   else {
+//     input.style.backgroundColor = "";
+//     input.style.border = "";
+//     payment();
+//     return;
+//   }
+// }
 
-function checkInput(){
-  var input = document.getElementById('modal-email');
-  if (input.value == null || input.value == ""){
-    input.style.backgroundColor = "#ffdad3";
-    input.style.border = "1px solid tomato";
-    return;
-  }
-  else {
-    input.style.backgroundColor = "";
-    input.style.border = "";
-    payment();
-    return;
-  }
-}
-
-function payment(){
-  document.getElementById('modal-1').style.display = "none";
-  document.getElementById('modal-2').style.display = "block";
-  document.getElementById('modal-3').style.display = "none";
-}
+// function payment(){
+//   document.getElementById('modal-1').style.display = "none";
+//   document.getElementById('modal-2').style.display = "block";
+//   document.getElementById('modal-3').style.display = "none";
+// }
 
 function creditCard(){
   document.getElementById('modal-1').style.display = "none";
   document.getElementById('modal-2').style.display = "none";
   document.getElementById('modal-3').style.display = "block";
 }
-
-function singleUseToken() {
-  var form = document.getElementById('payment-form');
-  form.submit(function(event) {
-    // Disable the submit button to prevent repeated clicks:
-    form.find('.submit').prop('disabled', true);
-
-    // Request a token from Stripe:
-    Stripe.card.createToken(form, stripeResponseHandler);
-
-    // Prevent the form from being submitted:
-    return false;
-  });
-};
-
-function stripeResponseHandler(status, response) {
-  // Grab the form:
-  var form = document.getElementById('payment-form');
-
-  if (response.error) { // Problem!
-
-    // Show the errors on the form:
-    form.find('.payment-errors').text(response.error.message);
-    form.find('.submit').prop('disabled', false); // Re-enable submission
-
-  } else { // Token was created!
-
-    // Get the token ID:
-    var token = response.id;
-
-    // Insert the token ID into the form so it gets submitted to the server:
-    form.appendChild(('<input type="hidden" name="stripeToken">').val(token));
-
-    // Submit the form:
-    form.get(0).submit();
-  }
-};
